527eb545fac5604fc7f48240b28235ce6684f757
Joel Martinez <joelmartinez@gmail.com>
2012-05-15 00:33:58 -0400

    added async jsonhelper ... this will be used for the WebHelper interface once windows phone 7 project uses async

--- KhanViewer/Models/WebHelper.cs
+++ KhanViewer/Models/WebHelper.cs
@@ -1,11 +1,36 @@
 ﻿using System;
 using System.IO;
 using System.Net;
+using System.Net.Http;
 using System.Runtime.Serialization.Json;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace KhanViewer.Models
 {
+#if !WINDOWS_PHONE
+    public static class JsonHelper 
+    {
+        public static async Task<T> DeserializeObject<T>(string url) where T : class
+        {
+            return await DeserializeObject<T>(new Uri(url)); 
+        }
+
+    public static async Task<T> DeserializeObject<T>(Uri url) where T : class 
+    {
+        DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(T));
+        try 
+        {
+            HttpResponseMessage response = await new HttpClient().GetAsync(url);
+            return serializer.ReadObject(await response.Content.ReadAsStreamAsync()) as T;
+        } 
+        catch (Exception ex) 
+        {
+            return null;                 
+        }
+    }
+#endif
+
     public static class WebHelper
     {
         public static void Get(string url, Action<string> action, Action<Exception> error)
