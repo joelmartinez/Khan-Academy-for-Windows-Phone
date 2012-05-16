848406c3e6f70579826d9ef6f20e19851ae7f74e
Joel Martinez <joelmartinez@gmail.com>
2012-05-16 00:02:43 -0400

    minor syntax cleanup, unused usings

--- KhanViewer/Models/KhanAcademyApi.cs
+++ KhanViewer/Models/KhanAcademyApi.cs
@@ -1,8 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Runtime.Serialization;
-using System.Collections.Generic;
 
 namespace KhanViewer.Models
 {
--- KhanViewer/ViewModels/MainViewModel.cs
+++ KhanViewer/ViewModels/MainViewModel.cs
@@ -2,7 +2,6 @@
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
-using System.Windows;
 using GoogleAnalyticsTracker;
 using KhanViewer.Models;
 
