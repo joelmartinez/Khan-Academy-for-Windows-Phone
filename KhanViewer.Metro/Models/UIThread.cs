f5be743e3c98d23a57f1a65bb4e6408f93925d8d
Joel Martinez <joelmartinez@gmail.com>
2012-05-22 22:58:13 -0400

    Removed #if defs from UIThread class

--- KhanViewer.Metro/Models/UIThread.cs
+++ KhanViewer.Metro/Models/UIThread.cs
@@ -1,18 +1,12 @@
 ﻿using System;
-using System.Windows;
 
-#if WINDOWS_PHONE
-using System.Windows.Threading;
-#else
 using Windows.UI.Core;
 using Windows.UI.Popups;
-#endif
 
 namespace KhanViewer.Models
 {
     public static class UIThread
     {
-#if !WINDOWS_PHONE
         private static CoreDispatcher Dispatcher;
 
         /// <summary>This should be called on the UI thread</summary>
@@ -45,31 +39,5 @@ namespace KhanViewer.Models
             Invoke(() => dialog.ShowAsync());
         }
 
-#else
-        private static readonly Dispatcher Dispatcher;
-
-        static UIThread()
-        {
-            // Store a reference to the current Dispatcher once per application
-            Dispatcher = Deployment.Current.Dispatcher;
-        }
-
-        /// <summary>
-        ///   Invokes the given action on the UI thread - if the current thread is the UI thread this will just invoke the action directly on
-        ///   the current thread so it can be safely called without the calling method being aware of which thread it is on.
-        /// </summary>
-        public static void Invoke(Action action)
-        {
-            if (Dispatcher.CheckAccess())
-                action.Invoke();
-            else
-                Dispatcher.BeginInvoke(action);
-        }
-
-        public static void MessageBox(string message)
-        {
-            Invoke(() => System.Windows.MessageBox.Show(message));
-        }
-#endif
     }
 }
